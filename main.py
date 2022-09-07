6942ad427f5e3e535d8e149877484a7cdc9687f5
Isaac-To <isaacto3890@gmail.com>
2022-09-06 22:46:42 -0700

    definte variable as task or not task

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def query_post():
         # collect results
         results = await asyncio.gather(*resultsTasks)
         interlacedResults = await interlace(results)
-        combinedSearchResultsTask = asyncio.create_task(
+        combinedSearchResults = asyncio.create_task(
             relevancyByOccurances(interlacedResults))  # type: ignore
         # load dependent widget sources
         # check if criteria to run widget is met
@@ -122,7 +122,7 @@ async def query_post():
             widgetTasks.append(asyncio.create_task(
                 wikipediaInSearch(interlacedResults)))
         # sort results
-        combinedSearchResults = await combinedSearchResultsTask
+        combinedSearchResults = await combinedSearchResults
         # start assembling HTML for results
         resultsHTML = asyncio.create_task(resultsToHTML(combinedSearchResults))
         # layering
